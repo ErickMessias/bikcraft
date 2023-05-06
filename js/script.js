a3ab7d2976cc3f33b5314af03d4d878efb6429e9
ErickMessias <erick.messias182@gmail.com>
2023-05-06 16:58:18 -0300

    inserindo animações ao site

--- js/script.js
+++ js/script.js
@@ -48,22 +48,25 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 
-
 //galeria de imagens
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
-const galeriaContainer = document.querySelector('.bicicleta-imagens');
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
 function trocarImagem(event) {
   const img = event.currentTarget;
-  const media = matchMedia('(min-width: 1000px').matches;
+  const media = matchMedia("(min-width: 1000px").matches;
   if (media) {
-    galeriaContainer.prepend(img)
+    galeriaContainer.prepend(img);
   }
-  
 }
 
 function eventosGaleria(img) {
-  img.addEventListener('click', trocarImagem);
+  img.addEventListener("click", trocarImagem);
 }
 
-galeria.forEach(eventosGaleria)
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
