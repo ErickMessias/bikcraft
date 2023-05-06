6b17398a3c86f71cdd3b9b7017f1f1b2df4f075f
ErickMessias <erick.messias182@gmail.com>
2023-05-06 15:57:33 -0300

    deixando perguntas selecionadas

--- js/script.js
+++ js/script.js
@@ -20,9 +20,30 @@ function ativarProduto(parametro) {
   if (elemento) {
     elemento.checked = true;
   }
-  
+
   console.log(elemento);
 }
 
 parametros.forEach(ativarProduto);
-console.log(parametros)
+console.log(parametros);
+
+// Peguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
